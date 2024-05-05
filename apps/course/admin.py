e328d68ed7df228fb14646d248c4f016715f7cb5
abedzalloum <abdalhafeez@sitech.me>
2024-05-05 13:25:32 +0300

    add Courses and StudentCourse table on admin dashboard

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.course.models import (
+    Courses,
+    StudentCourse
+)
 
-# Register your models here.
+admin.site.register(Courses)
+admin.site.register(StudentCourse)
